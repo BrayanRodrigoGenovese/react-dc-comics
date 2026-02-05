a780fc2c48fc90d00086ce0de3f701e8b579f243
Brayan <bittolo.8bit@gmail.com>
2026-02-05 04:32:35 +0100

    customizing the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,93 @@
 export default function Header() {
   return (
     <>
-      <header>
-        <ul>
-          <li>CHARACTERS</li>
-          <li>COMICS</li>
-          <li>MOVIES</li>
-          <li>TV</li>
-          <li>GAMES</li>
-          <li>COLLECTIBLES</li>
-          <li>VIDEOS</li>
-          <li>FANS</li>
-          <li>NEWS</li>
-          <li>SHOP</li>
-        </ul>
-      </header>
+      <div className="container">
+        <header className="d-flex flex-wrap justify-content-center align-items-center py-4">
+          <img src="/dc-logo.png" className="me-auto" alt="" />
+          <ul className="nav">
+            <li>
+              <a
+                href="#"
+                className="nav-link px-2 fw-bold text-secondary condensed-text"
+              >
+                CHARACTERS
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="nav-link px-2 fw-bold text-secondary condensed-text"
+              >
+                COMICS
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="nav-link px-2 fw-bold text-secondary condensed-text"
+              >
+                MOVIES
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="nav-link px-2 fw-bold text-secondary condensed-text"
+              >
+                TV
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="nav-link px-2 fw-bold text-secondary condensed-text"
+              >
+                GAMES
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="nav-link px-2 fw-bold text-secondary condensed-text"
+              >
+                COLLECTIBLES
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="nav-link px-2 fw-bold text-secondary condensed-text"
+              >
+                VIDEOS
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="nav-link px-2 fw-bold text-secondary condensed-text"
+              >
+                FANS
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="nav-link px-2 fw-bold text-secondary condensed-text"
+              >
+                NEWS
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="nav-link px-2 fw-bold text-secondary condensed-text"
+              >
+                SHOP
+              </a>
+            </li>
+          </ul>
+        </header>
+      </div>
     </>
   );
 }
