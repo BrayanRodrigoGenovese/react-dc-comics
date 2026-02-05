ad82d9def56347eb1cb264986e264ab5fab75fb4
Brayan <bittolo.8bit@gmail.com>
2026-02-05 04:33:11 +0100

    customizing the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,103 +2,262 @@ export default function Footer() {
   return (
     <>
       <footer>
-        <div className="container">
-          <ul>
-            <li>
-              <img src="/buy-comics-digital-comics.png" alt="" />
-              DIGITAL COMICS
-            </li>
-            <li>
-              <img src="/buy-comics-merchandise.png" alt="" />
-              DC MERCHANDISE
-            </li>
-            <li>
-              <img src="/buy-comics-subscriptions.png" alt="" />
-              SUBSCRIPTION
-            </li>
-            <li>
-              <img src="/buy-comics-shop-locator.png" alt="" />
-              COMIC SHOP LOCATOR
-            </li>
-            <li>
-              <img src="/buy-dc-power-visa.svg" alt="" />
-              DC POWER VISA
-            </li>
-          </ul>
-        </div>
-
-        <div className="container">
-          <div>
-            <h2>DC COMICS</h2>
-            <ul>
-              <li>Characters</li>
-              <li>Comics</li>
-              <li>Movies</li>
-              <li>TV</li>
-              <li>Games</li>
-              <li>Videos</li>
-              <li>News</li>
-            </ul>
-          </div>
-
-          <div>
-            <h2>SHOP</h2>
-            <ul>
-              <li>Shop DC</li>
-              <li>Shop DC Collectibles</li>
-            </ul>
-          </div>
-
-          <div>
-            <h2>DC</h2>
-            <ul>
-              <li>Terms Of Use</li>
-              <li>Privacy policy (New)</li>
-              <li>Ad Choices</li>
-              <li>Advertising</li>
-              <li>Jobs</li>
-              <li>Subscriptions</li>
-              <li>Talents Workshops</li>
-              <li>CPSC Certificates</li>
-              <li>Ratings</li>
-              <li>Shop Help</li>
-              <li>Contact Us</li>
-            </ul>
-          </div>
-
-          <div>
-            <h2>SITES</h2>
-            <ul>
-              <li>DC</li>
-              <li>MAD Magazine</li>
-              <li>DC Kids</li>
-              <li>DC Universe</li>
-              <li>DC Power Visa</li>
-            </ul>
+        <div id="usefull-info">
+          <div className="container">
+            <div className="row py-5">
+              <ul className="nav justify-content-center align-items-center">
+                <li className="nav-item text-white px-4">
+                  <img
+                    src="/buy-comics-digital-comics.png"
+                    className="pe-2"
+                    height={60}
+                    alt=""
+                  />
+                  DIGITAL COMICS
+                </li>
+                <li className="nav-item text-white px-4">
+                  <img
+                    src="/buy-comics-merchandise.png"
+                    className="pe-2"
+                    height={60}
+                    alt=""
+                  />
+                  DC MERCHANDISE
+                </li>
+                <li className="nav-item text-white px-4">
+                  <img
+                    src="/buy-comics-subscriptions.png"
+                    className="pe-2"
+                    height={60}
+                    alt=""
+                  />
+                  SUBSCRIPTION
+                </li>
+                <li className="nav-item text-white px-4">
+                  <img
+                    src="/buy-comics-shop-locator.png"
+                    className="pe-2"
+                    height={60}
+                    alt=""
+                  />
+                  COMIC SHOP LOCATOR
+                </li>
+                <li className="nav-item text-white px-4">
+                  <img
+                    src="/buy-dc-power-visa.svg"
+                    className="pe-2"
+                    width={70}
+                    alt=""
+                  />
+                  DC POWER VISA
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
 
-        <div className="container">
-          <div>
-            <button>SIGN-UP NOW!</button>
-            FOLLOW US
-            <ul>
-              <li>
-                <img src="/footer-facebook.png" alt="" />
-              </li>
-              <li>
-                <img src="/footer-twitter.png" alt="" />
-              </li>
-              <li>
-                <img src="/footer-youtube.png" alt="" />
-              </li>
-              <li>
-                <img src="/footer-pinterest.png" alt="" />
-              </li>
-              <li>
-                <img src="/footer-periscope.png" alt="" />
-              </li>
-            </ul>
+        <div id="about-us">
+          <div className="container">
+            <div className="row">
+              <div className="col-6 py-5">
+                <div className="row ">
+                  {/* Double Column */}
+                  <div className="flex-column col-4">
+                    {/* DC COMICS */}
+                    <div className="pb-3">
+                      <h2 className="fw-bold text-white condensed-text">
+                        DC COMICS
+                      </h2>
+                      <ul className="nav flex-column">
+                        <li className="nav-item text-secondary">
+                          <a href="#" className="nav-link p-0 text-secondary">
+                            Characters
+                          </a>
+                        </li>
+                        <li className="nav-item text-secondary">
+                          <a href="#" className="nav-link p-0 text-secondary">
+                            Comics
+                          </a>
+                        </li>
+                        <li className="nav-item text-secondary">
+                          <a href="#" className="nav-link p-0 text-secondary">
+                            Movies
+                          </a>
+                        </li>
+                        <li className="nav-item text-secondary">
+                          <a href="#" className="nav-link p-0 text-secondary">
+                            TV
+                          </a>
+                        </li>
+                        <li className="nav-item text-secondary">
+                          <a href="#" className="nav-link p-0 text-secondary">
+                            Games
+                          </a>
+                        </li>
+                        <li className="nav-item text-secondary">
+                          <a href="#" className="nav-link p-0 text-secondary">
+                            Videos
+                          </a>
+                        </li>
+                        <li className="nav-item text-secondary">
+                          <a href="#" className="nav-link p-0 text-secondary">
+                            News
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                    {/* SHOP */}
+                    <div>
+                      <h2 className="fw-bold text-white condensed-text">
+                        SHOP
+                      </h2>
+                      <ul className="nav flex-column">
+                        <li className="nav-item text-secondary">
+                          <a href="#" className="nav-link p-0 text-secondary">
+                            Shop DC
+                          </a>
+                        </li>
+                        <li className="nav-item text-secondary">
+                          <a href="#" className="nav-link p-0 text-secondary">
+                            Shop DC Collectibles
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  {/* DC */}
+                  <div className="col-4">
+                    <h2 className="fw-bold text-white condensed-text">DC</h2>
+                    <ul className="nav flex-column">
+                      <li className="nav-item text-secondary">
+                        <a href="#" className="nav-link p-0 text-secondary">
+                          Terms Of Use
+                        </a>
+                      </li>
+                      <li className="nav-item text-secondary">
+                        <a href="#" className="nav-link p-0 text-secondary">
+                          Privacy policy (New)
+                        </a>
+                      </li>
+                      <li className="nav-item text-secondary">
+                        <a href="#" className="nav-link p-0 text-secondary">
+                          Ad Choices
+                        </a>
+                      </li>
+                      <li className="nav-item text-secondary">
+                        <a href="#" className="nav-link p-0 text-secondary">
+                          Advertising
+                        </a>
+                      </li>
+                      <li className="nav-item text-secondary">
+                        <a href="#" className="nav-link p-0 text-secondary">
+                          Jobs
+                        </a>
+                      </li>
+                      <li className="nav-item text-secondary">
+                        <a href="#" className="nav-link p-0 text-secondary">
+                          Subscriptions
+                        </a>
+                      </li>
+                      <li className="nav-item text-secondary">
+                        <a href="#" className="nav-link p-0 text-secondary">
+                          Talents Workshops
+                        </a>
+                      </li>
+                      <li className="nav-item text-secondary">
+                        <a href="#" className="nav-link p-0 text-secondary">
+                          CPSC Certificates
+                        </a>
+                      </li>
+                      <li className="nav-item text-secondary">
+                        <a href="#" className="nav-link p-0 text-secondary">
+                          Ratings
+                        </a>
+                      </li>
+                      <li className="nav-item text-secondary">
+                        <a href="#" className="nav-link p-0 text-secondary">
+                          Shop Help
+                        </a>
+                      </li>
+                      <li className="nav-item text-secondary">
+                        <a href="#" className="nav-link p-0 text-secondary">
+                          Contact Us
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  {/* SITES */}
+                  <div className="col-4">
+                    <h2 className="fw-bold text-white condensed-text">SITES</h2>
+                    <ul className="nav flex-column">
+                      <li className="nav-item text-secondary">
+                        <a href="#" className="nav-link p-0 text-secondary">
+                          DC
+                        </a>
+                      </li>
+                      <li className="nav-item text-secondary">
+                        <a href="#" className="nav-link p-0 text-secondary">
+                          MAD Magazine
+                        </a>
+                      </li>
+                      <li className="nav-item text-secondary">
+                        <a href="#" className="nav-link p-0 text-secondary">
+                          DC Kids
+                        </a>
+                      </li>
+                      <li className="nav-item text-secondary">
+                        <a href="#" className="nav-link p-0 text-secondary">
+                          DC Universe
+                        </a>
+                      </li>
+                      <li className="nav-item text-secondary">
+                        <a href="#" className="nav-link p-0 text-secondary">
+                          DC Power Visa
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className="col-6">
+                <img
+                  src="/dc-logo-bg.png"
+                  className="img-fluid"
+                  id="dc-logo-bg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="follow-us">
+          <div className="container">
+            <div className="d-flex flex-wrap align-items-center justify-content-center py-5">
+              <button type="button" className="me-auto" id="sign-up-button">
+                SIGN-UP NOW!
+              </button>
+              <div className="d-flex flex-wrap">
+                <h2 className="fw-bold condensed-text me-3">FOLLOW US</h2>
+                <ul className="nav">
+                  <li className="nav-item px-2">
+                    <img src="/footer-facebook.png" alt="" />
+                  </li>
+                  <li className="nav-item px-2">
+                    <img src="/footer-twitter.png" alt="" />
+                  </li>
+                  <li className="nav-item px-2">
+                    <img src="/footer-youtube.png" alt="" />
+                  </li>
+                  <li className="nav-item px-2">
+                    <img src="/footer-pinterest.png" alt="" />
+                  </li>
+                  <li className="nav-item px-2">
+                    <img src="/footer-periscope.png" alt="" />
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </footer>
