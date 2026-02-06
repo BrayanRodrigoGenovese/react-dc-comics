81d635bc161c9c4386449d27d7aaa18576bae472
Brayan <bittolo.8bit@gmail.com>
2026-02-06 03:49:21 +0100

    second exercise

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,50 +6,60 @@ export default function Footer() {
           <div className="container">
             <div className="row py-5">
               <ul className="nav justify-content-center align-items-center">
-                <li className="nav-item text-white px-4">
-                  <img
-                    src="/buy-comics-digital-comics.png"
-                    className="pe-2"
-                    height={60}
-                    alt=""
-                  />
-                  DIGITAL COMICS
+                <li className="nav-item text-white px-3">
+                  <a href="#" className="nav-link text-white">
+                    <img
+                      src="/buy-comics-digital-comics.png"
+                      className="pe-2"
+                      height={60}
+                      alt=""
+                    />
+                    DIGITAL COMICS
+                  </a>
                 </li>
-                <li className="nav-item text-white px-4">
-                  <img
-                    src="/buy-comics-merchandise.png"
-                    className="pe-2"
-                    height={60}
-                    alt=""
-                  />
-                  DC MERCHANDISE
+                <li className="nav-item text-white px-3">
+                  <a href="#" className="nav-link text-white">
+                    <img
+                      src="/buy-comics-merchandise.png"
+                      className="pe-2"
+                      height={60}
+                      alt=""
+                    />
+                    DC MERCHANDISE
+                  </a>
                 </li>
-                <li className="nav-item text-white px-4">
-                  <img
-                    src="/buy-comics-subscriptions.png"
-                    className="pe-2"
-                    height={60}
-                    alt=""
-                  />
-                  SUBSCRIPTION
+                <li className="nav-item text-white px-3">
+                  <a href="#" className="nav-link text-white">
+                    <img
+                      src="/buy-comics-subscriptions.png"
+                      className="pe-2"
+                      height={60}
+                      alt=""
+                    />
+                    SUBSCRIPTION
+                  </a>
                 </li>
-                <li className="nav-item text-white px-4">
-                  <img
-                    src="/buy-comics-shop-locator.png"
-                    className="pe-2"
-                    height={60}
-                    alt=""
-                  />
-                  COMIC SHOP LOCATOR
+                <li className="nav-item text-white px-3">
+                  <a href="#" className="nav-link text-white">
+                    <img
+                      src="/buy-comics-shop-locator.png"
+                      className="pe-2"
+                      height={60}
+                      alt=""
+                    />
+                    COMIC SHOP LOCATOR
+                  </a>
                 </li>
-                <li className="nav-item text-white px-4">
-                  <img
-                    src="/buy-dc-power-visa.svg"
-                    className="pe-2"
-                    width={70}
-                    alt=""
-                  />
-                  DC POWER VISA
+                <li className="nav-item text-white px-3">
+                  <a href="#" className="nav-link text-white">
+                    <img
+                      src="/buy-dc-power-visa.svg"
+                      className="pe-2"
+                      width={70}
+                      alt=""
+                    />
+                    DC POWER VISA
+                  </a>
                 </li>
               </ul>
             </div>
